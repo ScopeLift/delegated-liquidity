c411f78ec4617eef856a702b4f275da78615b70e
John Feras <jferas@ferasinfotech.com>
2023-10-18 12:58:23 -0400

    Shorten remappings.txt

--- src/router/UniswapV4Caller.sol
+++ src/router/UniswapV4Caller.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
