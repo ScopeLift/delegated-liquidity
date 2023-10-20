19f2bf2f5a017e97d851e8c4fda6c7e8936e7233
keating <keating.dev@protonmail.com>
2023-10-20 16:02:47 -0400

    Initial test base class

---
+++ test/mocks/GovernorTokenMock.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20Votes} from "openzeppelin/token/ERC20/extensions/ERC20Votes.sol";
+import {ERC20Permit} from "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+
+/// @notice An ERC20Votes token to help test the L2 voting system
+contract GovernorTokenMock is ERC20Votes {
+  constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) ERC20Permit(_name) {}
+
+  /// @dev Mints tokens to an address to help test bridging and voting.
+  /// @param account The address of where to mint the tokens.
+  /// @param amount The amount of tokens to mint.
+  function mint(address account, uint256 amount) public {
+    _mint(account, amount);
+    delegate(account);
+  }
+
+  /**
+   * @dev Not really needed, except as compatibility shim for our GovernorMock (which is
+   * ERC20VotesComp)
+   */
+  function getPriorVotes(address account, uint256 blockNumber)
+    external
+    view
+    virtual
+    returns (uint256)
+  {
+    return getPastVotes(account, blockNumber);
+  }
+}
